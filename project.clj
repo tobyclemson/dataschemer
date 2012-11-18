e0e6b211f1bf8297cba898d6d479e5c13011768c
Toby Clemson <tobyclemson@gmail.com>
2012-11-18 10:27:55 -0500

    Upgrading to Clojure 1.4.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject dataschemer "0.1.0-SNAPSHOT"
   :description "Data schema declaration, generation and verification framework."
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :test-selectors { :functional :functional })
